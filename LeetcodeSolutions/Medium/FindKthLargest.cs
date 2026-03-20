71b8e92d3fb375560f6378f8f34432c4f1a3fa1d
micro <microcropoc@yandex.ru>
2026-03-20 23:10:57 +0300

    min-heap

---
+++ LeetcodeSolutions/Medium/FindKthLargest.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeTests.Medium;
+
+public class FindKthLargest
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = FindKthLargestPQ([3, 2, 3, 1, 2, 4, 5, 5, 6], 4);
+
+        Assert.Equal(4, result);
+    }
+
+    public int FindKthLargestSimple(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        return nums[nums.Length - k];
+    }
+
+    public int FindKthLargestPQ(int[] nums, int k)
+    {
+        var pq = new PriorityQueue<int, int>();
+
+        foreach (var n in nums)
+        {
+            pq.Enqueue(n, n);
+
+            if(pq.Count > k)
+                pq.Dequeue();
+        }
+
+        return pq.Dequeue();
+    }
+}
---
+++ LeetcodeSolutions/Medium/TopKFrequent.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCodeTests.Medium;
+
+public class TopKFrequent
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = TopKFrequentPQ([1, 1, 1, 2, 2, 3], 2);
+
+        Assert.Equal([1, 2], result);
+    }
+
+    public int[] TopKFrequentPQ(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+
+        foreach (int i in nums)
+        {
+            if(dict.ContainsKey(i))
+                dict[i]++;
+            else 
+                dict[i] = 1;
+        }
+
+        var pq = new PriorityQueue<int, int>();
+
+        foreach (var kv in dict)
+        {
+            pq.Enqueue(kv.Key, kv.Value);
+
+            if(pq.Count > k)
+                pq.Dequeue();
+        }
+
+        var result = new int[k];
+
+        for(int i = k - 1; i >= 0; i--)
+        {
+            result[i] = pq.Dequeue();
+        }
+
+        return result;
+    }
+}
