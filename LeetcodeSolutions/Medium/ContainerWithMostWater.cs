7602a26f4486920c6e565e79ff7055bf8b97840a
micro <microcropoc@yandex.ru>
2026-03-21 15:42:27 +0300

    two pointer

---
+++ LeetcodeSolutions/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeTests.Medium;
+
+public class ContainerWithMostWater
+{
+    [Fact]
+    public void Test1()
+    {
+        int[] height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+        var result = MaxArea(height);
+
+        Assert.Equal(49, result);
+    }
+
+    public int MaxArea(int[] height)
+    {
+        var left = 0;
+        var right = height.Length - 1;
+
+        var maxArea = 0;
+
+        while (left < right)
+        {
+            maxArea = Math.Max(Math.Min(height[left], height[right]) * (right - left), maxArea);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+}
