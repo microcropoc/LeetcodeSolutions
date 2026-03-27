71cf3f800f878d4de0fcc45e24e2e9934d312406
micro <microcropoc@yandex.ru>
2026-03-27 15:45:31 +0300

    sliding window

---
+++ LeetcodeSolutions/Medium/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeTests.Common;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(3, LengthOfLongestSubstring("pwwkew"));
+    }
+    public int LengthOfLongestSubstring(string s)
+    {
+        var hash = new HashSet<char>();
+        var maxLenght = 0;
+        var left = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            while (hash.Contains(s[i]))
+            {
+                hash.Remove(s[left]);
+                left++;
+            }
+
+            hash.Add(s[i]);
+
+            maxLenght = Math.Max(maxLenght, i - left + 1);
+        }
+
+        return maxLenght;
+    }
+}
---
+++ LeetcodeSolutions/Medium/MinimumSizeSubarraySum.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeTests.Common;
+
+public class MinimumSizeSubarraySum
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(2, MinSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
+    }
+
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        var left = 0;
+        var sum = 0;
+        int minLength = int.MaxValue;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            sum += nums[right];
+
+            while (sum >= target)
+            {
+                minLength = Math.Min(minLength, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLength == int.MaxValue ? 0 : minLength;
+    }
+}
