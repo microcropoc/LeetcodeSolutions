37bf66fdfcfea0f7bee11214d9e9167c945d9ab0
micro <microcropoc@yandex.ru>
2026-03-27 15:45:50 +0300

    easy

---
+++ LeetcodeSolutions/Easy/MajorityElement.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeTests.Easy;
+
+public class MajorityElement
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(2, MajorityElementBest([2, 2, 1, 1, 1, 2, 2]));
+    }
+
+    public int MajorityElementLINQ(int[] nums)
+    {
+        return nums.GroupBy(i => i).MaxBy(i => i.Count())!.Key;
+    }
+
+    public int MajorityElementSorted(int[] nums)
+    {
+        Array.Sort(nums);
+
+        return nums[nums.Length / 2];
+    }
+
+    public int MajorityElementHash(int[] nums)
+    {
+        Array.Sort(nums);
+
+        return nums[nums.Length / 2];
+    }
+
+    public int MajorityElementBest(int[] nums)
+    {
+        var curr = 0;
+        var count = 0;
+
+        foreach (var item in nums)
+        {
+            if (count == 0)
+            {
+                curr = item;
+                count = 1;
+            }
+            else
+            {
+                if (curr == item)
+                    count++;
+                else
+                    count--;
+            }
+        }
+
+        return curr;
+    }
+}
---
+++ LeetcodeSolutions/Easy/RansomNote.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeTests.Easy;
+
+public class RansomNote
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(CanConstruct("aa", "aab"));
+    }
+
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var lit = new int[26];
+
+        foreach (var item in magazine)
+        {
+            lit[item - 'a']++;
+        }
+
+        foreach(var item in ransomNote)
+        {
+            lit[item - 'a']--;
+
+            if(lit[item - 'a'] < 0)
+                return false;
+        }
+
+        return true;
+    }
+}
